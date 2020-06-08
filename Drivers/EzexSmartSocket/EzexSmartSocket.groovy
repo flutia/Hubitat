c12e7a26ca91bd8913667042a08291643667e81e
flutia <flutia.kr@gmail.com>
2020-06-08 23:59:16 +0900

    fit to common code convention

--- Drivers/EzexSmartSocket/EzexSmartSocket.groovy
+++ Drivers/EzexSmartSocket/EzexSmartSocket.groovy
@@ -1,6 +1,6 @@
 /*
  * eZEX C2O Smart Socket for Hubitat (2 sockets, E210-KR210Z1-HA) - v1.0.0
- *  
+ *
  *  github: Euiho Lee (flutia)
  *  email: flutia@naver.com
  *  Date: 2020-06-08
@@ -19,49 +19,44 @@
  *  under the License.
  */
 metadata {
-    definition(name: "eZEX Smart Socket", namespace: "flutia", author: "flutia") {
-        capability "Actuator"
-        capability "Configuration"
-        capability "Refresh"
-        capability "Health Check"
-        
+    definition(name: 'eZEX Smart Socket', namespace: 'flutia', author: 'flutia') {
+        capability 'Actuator'
+        capability 'Configuration'
+        capability 'Refresh'
+        capability 'HealthCheck'
+
         // switch/outlet은 둘 다 on/off command를 정의한다.
-        capability "Switch"
-        capability "Outlet"
-        
-        capability "EnergyMeter" // attribute:energy - 전체 사용량
-        capability "PowerMeter" // attribute:power - 현재 전력 (W)
-        capability "VoltageMeasurement" // attribute:voltage  - 현재 전압
-        
-        // ezex에서는 제공하지만 Hubitat Capability에서 제공하지 않는 값들. 
-        attribute "InstantaneousPower", "number"    // 순시 전력
-        attribute "lineCurrent", "number"    // 현재 전류
-        attribute "powerCutOff", "string"        // 전력 차단
-        attribute "powerFactor", "number"        // 역률
-        attribute "lock", "string"            // 잠금 
-        
+        capability 'Switch'
+        capability 'Outlet'
+
+        capability 'EnergyMeter' // attribute:energy - 전체 사용량
+        capability 'PowerMeter' // attribute:power - 현재 전력 (W)
+        capability 'VoltageMeasurement' // attribute:voltage  - 현재 전압
+
+        // ezex에서는 제공하지만 Hubitat Capability에서 제공하지 않는 값들.
+        attribute 'InstantaneousPower', 'number'    // 순시 전력
+        attribute 'lineCurrent', 'number'    // 현재 전류
+        attribute 'powerCutOff', 'string'        // 전력 차단
+        attribute 'powerFactor', 'number'        // 역률
+        attribute 'lock', 'string'            // 잠금
+
         // lock/unlock 기능은 Zigbee Command로만 제어가 가능하다. (장치상에는 lock/unlock 기능이 없음)
-        command "lock"
-        command "unlock"
-        
-        fingerprint profileId: "0104", deviceId: "0051", inClusters: "0000, 0003, 0004, 0006, 0B04, 0702", outClusters: "0019", model: "E210-KR210Z1-HA"
+        command 'lock'
+        command 'unlock'
+
+        fingerprint profileId: '0104', deviceId: '0051', inClusters: '0000, 0003, 0004, 0006, 0B04, 0702', outClusters: '0019', model: 'E210-KR210Z1-HA'
     }
     preferences {
-        section() {
-            input name: 'prefIntervalMin', type:'number', title: '최소 리포팅 주기(단위:초):', defaultValue: 30, range: '1..600', required: false
-            input name: 'prefIntervalMax', type:'number', title: '최대 리포팅 주기(단위:초):', defaultValue: 60, range: '1..600', required: false
-            input name: 'prefMinDeltaPower', type:'enum', title: '전력량(단위:W)이 다음 값보다 많이 바뀌면 리포팅:', options: ['1', '5', '10', '15', '25', '30', '40', '50', '70', '100'], defaultValue: '15', required: false
-            input name: 'prefMinDeltaEnergy', type:'enum', title: '전력량(단위:Wh)이 다음 값보다 많이 바뀌면 리포팅:', options: ['1', '2', '3', '5', '10', '20'], defaultValue: '10', required: false
-            input name: 'prefMinDeltaLineCurrent', type:'number', title: '전류량(단위:A)이 다음 값보다 많이 바뀌면 리포팅:', defaultValue: 1, range: '1..10', required: false
-            input name: 'prefMinDeltaVoltage', type:'number', title: '전압(단위:V)이 다음 값보다 많이 바뀌면 리포팅:', defaultValue: 1, range: '1..10', required: false
-        }
-        section() {
-            input name: 'prefIsDebugEnabled', type: 'bool', title: 'Enable debug logging', defaultValue: false
-            input name: 'prefIsInfoEnabled', type: 'bool', title: 'Enable info logging', defaultValue: true
-        }
+        input name: 'prefIntervalMin', type:'number', title: '최소 리포팅 주기(단위:초):', defaultValue: 30, range: '1..600', required: false
+        input name: 'prefIntervalMax', type:'number', title: '최대 리포팅 주기(단위:초):', defaultValue: 60, range: '1..600', required: false
+        input name: 'prefMinDeltaPower', type:'enum', title: '전력량(단위:W)이 다음 값보다 많이 바뀌면 리포팅:', options: ['1', '5', '10', '15', '25', '30', '40', '50', '70', '100'], defaultValue: '15', required: false
+        input name: 'prefMinDeltaEnergy', type:'enum', title: '전력량(단위:Wh)이 다음 값보다 많이 바뀌면 리포팅:', options: ['1', '2', '3', '5', '10', '20'], defaultValue: '10', required: false
+        input name: 'prefMinDeltaLineCurrent', type:'number', title: '전류량(단위:A)이 다음 값보다 많이 바뀌면 리포팅:', defaultValue: 1, range: '1..10', required: false
+        input name: 'prefMinDeltaVoltage', type:'number', title: '전압(단위:V)이 다음 값보다 많이 바뀌면 리포팅:', defaultValue: 1, range: '1..10', required: false
 
+        input name: 'prefIsDebugEnabled', type: 'bool', title: 'Enable debug logging', defaultValue: false
+        input name: 'prefIsInfoEnabled', type: 'bool', title: 'Enable info logging', defaultValue: true
     }
-
 }
 
 def installed() {
@@ -79,16 +74,16 @@ def updated() {
 }
 
 def configure() {
-    logD("configure operation")
-    
+    logD('configure operation')
+
     def cmd = getConfigureCommand()
     logD("configure command. ${cmd}")
     return cmd
 }
 
 def refresh() {
-    logD("Refresh operation")
-    
+    logD('Refresh operation')
+
     def cmds = new hubitat.device.HubMultiAction()
     cmds.add(getOnOffRefreshCommand())
     cmds.add(getPowerRefreshCommand())
@@ -100,7 +95,7 @@ def refresh() {
     cmds.add(getVoltageRefreshCommand())
     cmds.add(getPowerCutOffRefreshCommand())
     cmds.add(getPowerFactorRefreshCommand())
-    
+
     sendHubCommand(cmds)
 }
 
@@ -108,117 +103,104 @@ def ping() {
     return zigbee.onOffRefresh()
 }
 
-
 def parse(String description) {
     def parseMap = zigbee.parseDescriptionAsMap(description)
     def event = zigbee.getEvent(description)
-    
+
     def clusterId
     def attrId
     def commandData
-    if( parseMap != null ) {
+    if (parseMap != null) {
         clusterId = parseMap.cluster ? parseMap.cluster : parseMap.clusterId
         clusterId = (clusterId != null) ? clusterId.toUpperCase() : null
         attrId = parseMap.attrId != null ? parseMap.attrId.toUpperCase() : null
-        if( parseMap.data != null ) {
+        if (parseMap.data != null) {
             commandData = String.valueOf(parseMap.data[0])
         }
     }
     logD("parseMap: ${parseMap}")
 
     def eventStack = []
-    if( parseMap != null ) {
+    if (parseMap != null)  {
         def forceReturn = false
 
-        if( clusterId == "0006" ) {
-            if(attrId == "0000") { // on/off
+        if (clusterId == '0006') {
+            if (attrId == '0000') { // on/off
                 def intOnOff = zigbee.convertHexToInt(parseMap.value)
-                def onOffValue = (intOnOff == 1) ? "on":"off"
+                def onOffValue = (intOnOff == 1) ? 'on' : 'off'
                 logI("on/off: ${onOffValue}")
-                eventStack.push(createEvent(name:"switch", value:onOffValue))
-                
-             } else if( attrId == "0010" ) { // refresh로 들어온 lock/unlock
+                eventStack.push(createEvent(name:'switch', value:onOffValue))
+             } else if (attrId == '0010') { // refresh로 들어온 lock/unlock
                 def value = zigbee.convertHexToInt(parseMap.value)
-                def strValue = (value == 1) ? "locked" : "unlocked"
+                def strValue = (value == 1) ? 'locked' : 'unlocked'
                 logI("lock: ${strValue}")
-                eventStack.push(createEvent( name: "lock", value: strValue))
-                                              
+                eventStack.push(createEvent( name: 'lock', value: strValue))
             } else {
                 log.warn "UNKNOWN for 0006: ${description}, ${parseMap}"
             }
-            
-        } else if( clusterId == "0B04" ) {
-            if( attrId == "0510" ) {  // 역률
+        } else if (clusterId == '0B04') {
+            if (attrId == '0510') {  // 역률
                 def powerFactor = zigbee.convertHexToInt(parseMap.value)
                 logI("powerFactor: ${powerFactor}")
-                eventStack.push(createEvent( name: "powerFactor", value: powerFactor))
-                
-            } else if( attrId == "050B") {  // 전력(W)
+                eventStack.push(createEvent( name: 'powerFactor', value: powerFactor))
+            } else if (attrId == '050B') {  // 전력(W)
                 def activePower = zigbee.convertHexToInt(parseMap.value)
                 logI("power: ${activePower}")
-                eventStack.push(createEvent( name: "power", value: activePower))
+                eventStack.push(createEvent( name: 'power', value: activePower))
             }
-            
-        } else if( clusterId == "0702" ) {
+        } else if (clusterId == '0702') {
             def renewWatt = false
             def simpleMeteringAttrProcessor = { theAttrId, value ->
-                if(theAttrId == "0000") { // 총 전력
+                if (theAttrId == '0000') { // 총 전력
                     def energyValue = calcElectricMeasure(value)
                     logI("energy: ${energyValue}")
-                    eventStack.push(createEvent(name: "energy", value: energyValue))
-                    
-                } else if( theAttrId == "0301") { // measure metering multiplier
+                    eventStack.push(createEvent(name: 'energy', value: energyValue))
+                } else if (theAttrId == '0301') { // measure metering multiplier
                     def intValue = zigbee.convertHexToInt(value)
                     state.meteringMultiplier = intValue
                     logD("meteringMultiplier: ${intValue}")
                     forceReturn = true
-                    
-                } else if( theAttrId == "0302") { // measure metering divisor
+                } else if (theAttrId == '0302') { // measure metering divisor
                     def intValue = zigbee.convertHexToInt(value)
                     state.meteringDivisor = intValue
                     logD("meteringDivisor: ${intValue}")
                     forceReturn = true
-                    
-                } else if( theAttrId == "0400") { // 순시전력: Instantaneous demand (or Instantaneous power)
+                } else if (theAttrId == '0400') { // 순시전력: Instantaneous demand (or Instantaneous power)
                     def instantPower = calcElectricMeasure(value)
                     logI("Instantaneous power: ${instantPower}")
-                    eventStack.push(createEvent(name: "InstantaneousPower", value: instantPower))
-                    
-                } else if(theAttrId == "0901") { // 전류
+                    eventStack.push(createEvent(name: 'InstantaneousPower', value: instantPower))
+                } else if (theAttrId == '0901') { // 전류
                     def current = calcCurrentValue(value)
                     logI("current: ${current}")
-                    eventStack.push(createEvent( name: "lineCurrent", value: current))
-                    
-                } else if(theAttrId == "0902") { // 전압
+                    eventStack.push(createEvent( name: 'lineCurrent', value: current))
+                } else if (theAttrId == '0902') { // 전압
                     def voltage = calcCurrentValue(value)
                     logI("voltage: ${voltage}")
-                    eventStack.push(createEvent( name: "voltage", value: voltage))
-                    
-                } else if(theAttrId == "0905" ) { // 대기전력 차단 여부
-                    def powerCutOffEnabled = value == "01" ? "enabled" : "disabled"
+                    eventStack.push(createEvent( name: 'voltage', value: voltage))
+                } else if (theAttrId == '0905') { // 대기전력 차단 여부
+                    def powerCutOffEnabled = value == '01' ? 'enabled' : 'disabled'
                     logD("powerCutOff: ${powerCutOffEnabled}")
-                    eventStack.push(createEvent( name: "powerCutOff", value: powerCutOffEnabled))
-                    
+                    eventStack.push(createEvent( name: 'powerCutOff', value: powerCutOffEnabled))
                 } else {
                     log.warn "Unhandle cluster: ${clusterId}, ${theAttrId}, ${value}, description is ${description}"
                 }
-            };
-            
+            }
+
             def attrs = parseMap.additionalAttrs // 이젝스 확장
-            if( attrs == null ) {    // 이젝스 확장 값이 없으면 개별 리포팅된 케이스이다.
+            if (attrs == null) {    // 이젝스 확장 값이 없으면 개별 리포팅된 케이스이다.
                 simpleMeteringAttrProcessor(attrId, parseMap.value)
             } else { // 자동 리포팅: 디바이스에서 리포팅하는 정보는 attrs에 배열로 담겨져 온다.
-                attrs.each { attr -> 
+                attrs.each { attr ->
                     simpleMeteringAttrProcessor(attr.attrId, attr.value)
                 }
             }
         }
-        
+
         // log.debug "eventStack: ${eventStack}, ${clusterId}, ${attrId}"
-        if(!eventStack.isEmpty()) {
+        if (!eventStack.isEmpty()) {
             return eventStack
         }
-        if( forceReturn ) {
+        if (forceReturn) {
             return
         }
     }
@@ -235,20 +217,19 @@ def on() {
     zigbee.on()
 }
 
-
 def lock() {
-    logD("lock operation")
+    logD('lock operation')
     def cmds = []
     cmds << "he cmd 0x${device.deviceNetworkId} 0x1 0x0006 0x10 {}"
-    cmds << "delay 100"
+    cmds << 'delay 100'
     return cmds
 }
 
 def unlock() {
-    logD("unlock operation")
+    logD('unlock operation')
     def cmds = []
     cmds << "he cmd 0x${device.deviceNetworkId} 0x1 0x0006 0x11 {}"
-    cmds << "delay 100"
+    cmds << 'delay 100'
     return cmds
 }
 
@@ -262,7 +243,6 @@ private getSafeNumberValueFromState(stateKey) {
     return calVal
 }
 
-
 void logD(String msg) {
     if (prefIsDebugEnabled) {
         log.debug(msg)
@@ -274,7 +254,6 @@ void logI(String msg) {
         log.info(msg)
     }
 }
-
 
 /**
  * on/off 여부를 얻는 커맨드
@@ -294,7 +273,6 @@ def getPowerRefreshCommand() {
     return cmds
 }
 
-
 /**
  * Lock 여부를 얻는 커맨드
  */
@@ -309,7 +287,7 @@ def getLockRefreshCommand() {
  */
 def getMeteringUnitRefreshCommand() {
     def cmds = new hubitat.device.HubMultiAction()
-    cmds.add(new hubitat.device.HubAction("he rattr 0x${device.deviceNetworkId} 0x1 0x0702 0x0301", hubitat.device.Protocol.ZIGBEE)) // 
+    cmds.add(new hubitat.device.HubAction("he rattr 0x${device.deviceNetworkId} 0x1 0x0702 0x0301", hubitat.device.Protocol.ZIGBEE)) //
     cmds.add(new hubitat.device.HubAction("he rattr 0x${device.deviceNetworkId} 0x1 0x0702 0x0302", hubitat.device.Protocol.ZIGBEE))
     return cmds
 }
@@ -387,13 +365,10 @@ def getConfigureCommand() {
     def energyConfig = zigbee.configureReporting(0x0702, 0x0000, 0x25, intervalMin, intervalMax, minDeltaEnergy) // 전체 전력
     def lineCurrentConfig = zigbee.configureReporting(0x0702, 0x0901, 0x22, intervalMin, intervalMax, minDelteLineCurrent) // 전류
     def voltageConfig = zigbee.configureReporting(0x0702, 0x0902, 0x22, intervalMin, intervalMax, minDelteVoltage) // 전압
-    
+
     def cmd = onOffConfig + powerConfig + powerFactorConfig + energyConfig + lineCurrentConfig + voltageConfig
     return cmd
 }
-
-
-
 
 /**
  * 기기로부터 온 전력 값(순시 전력, 누적 전력)을 계산한다.
@@ -401,10 +376,10 @@ def getConfigureCommand() {
 def calcElectricMeasure(value) {
     def intValue = value ?: 0
     intValue = intValue instanceof Number ? intValue : zigbee.convertHexToInt(intValue)
-    
+
     def multiplier = getSafeNumberValueFromState('meteringMultiplier')
     def divisor = getSafeNumberValueFromState('meteringDivisor')
-    if( divisor == 0 ) {
+    if (divisor == 0) {
         divisor = 1
     }
     def elecValue = intValue * multiplier / divisor * 1000
@@ -416,7 +391,7 @@ def calcElectricMeasure(value) {
  */
 def calcCurrentValue(value) {
     def current = 0
-    if( value ) {
+    if (value) {
         current = zigbee.convertHexToInt(value)
     }
     current = current / 1000
